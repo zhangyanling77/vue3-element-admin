69d9a48ae3dea9ac889488016bdca9d8bc51a127
zhangyanling77 <18783226594@163.com>
2021-05-26 16:07:21 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home'
+})
+</script>
