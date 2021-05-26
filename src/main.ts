f27898f6cc1c71ee09a88f3f1e4b6ce4467e81b2
zhangyanling77 <18783226594@163.com>
2021-05-26 15:34:48 +0800

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 初始化css
+import 'normalize.css/normalize.css';
+import installElementPlus from './plugins/element'
+
+const app = createApp(App)
+
+app
+  .use(store)
+  .use(router)
+  .use(installElementPlus)
+  .mount('#app')
