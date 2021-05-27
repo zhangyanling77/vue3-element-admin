d48fd16e6405bfadb51b7fe7ce337261009fd32a
zhangyanling77 <18783226594@163.com>
2021-05-27 14:55:01 +0800

    Create variables.scss.d.ts

---
+++ src/styles/variables.scss.d.ts
@@ -0,0 +1,13 @@
+export interface ScssVariables {
+  menuText: string;
+  menuActiveText: string;
+  subMenuActiveText: string;
+  menuBg: string;
+  subMenuBg: string;
+  subMenuHover: string;
+  sideBarWidth: string;
+}
+
+export const varibales: ScssVariables
+
+export default varibales
