a36155f96a7f437c771036f78af834e238ef5b9f
zhangyanling77 <18783226594@163.com>
2021-05-27 14:56:50 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 // 初始化css
-import 'normalize.css/normalize.css';
+import 'normalize.css/normalize.css'
 import installElementPlus from './plugins/element'
+import './styles/index.scss'
 
 const app = createApp(App)
 
