0b255663a973da518e5f8da639844e7f910e1583
zhangyanling77 <18783226594@163.com>
2021-05-27 15:05:25 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 // 初始化css
 import 'normalize.css/normalize.css'
 import installElementPlus from './plugins/element'
+import { ElMessage, ElNotification, ElMessageBox } from 'element-plus'
 import './styles/index.scss'
 
 const app = createApp(App)
@@ -14,3 +15,14 @@ app
   .use(router)
   .use(installElementPlus)
   .mount('#app')
+
+// vue 实例上挂载属性类型声明
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $message: typeof ElMessage;
+    $notify: typeof ElNotification;
+    $confirm: typeof ElMessageBox.confirm;
+    $alert: typeof ElMessageBox.alert;
+    $prompt: typeof ElMessageBox.prompt;
+  }
+}
