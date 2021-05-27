0341289bf4e69820dc006584311e8ed9b07604fd
zhangyanling77 <18783226594@163.com>
2021-05-27 14:53:01 +0800

    Update element.ts

--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -1,5 +1,5 @@
-import type { App } from 'vue';
-import { ElButton } from 'element-plus'
+import type { App } from 'vue'
+import { ElButton, ElMessage, ElMessageBox, ElNotification } from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 // Element Plus 组件内部默认使用英语
 // https://element-plus.gitee.io/#/zh-CN/component/i18n
@@ -8,18 +8,27 @@ import locale from 'element-plus/lib/locale'
 // Element Plus 直接使用了 Day.js 项目的时间日期国际化设置, 并且会自动全局设置已经导入的 Day.js 国际化配置。
 import 'dayjs/locale/zh-cn'
 
+export type Size = 'default' | 'medium' | 'small' | 'mini'
+
 export default (app: App): void => {
   locale.use(lang)
-  
   // 按需导入组件列表
   const components = [
     ElButton,
+    ElMessage,
+    ElMessageBox,
+    ElNotification
   ]
 
   components.forEach(component => {
     app.component(component.name, component)
   })
 
+  app.config.globalProperties.$message = ElMessage
+  app.config.globalProperties.$notify = ElNotification
+  app.config.globalProperties.$confirm = ElMessageBox.confirm
+  app.config.globalProperties.$alert = ElMessageBox.alert
+  app.config.globalProperties.$prompt = ElMessageBox.prompt
   // 全局配置 文档说明 https://element-plus.gitee.io/#/zh-CN/component/quickstart#quan-ju-pei-zhi
   // 该对象目前支持 size 与 zIndex 字段。size 用于改变组件的默认尺寸 small，zIndex 设置弹框的初始 z-index（默认值：2000）。
   app.config.globalProperties.$ELEMENT = {
